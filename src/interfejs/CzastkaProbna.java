d121eb0abdb13feb18ea87e0283bc3b77accac17
Piotr Wysocki <Piotrek05092000@gmail.com>
2021-04-28 12:56:43 +0200

    Textprompt + czastki

---
+++ src/interfejs/CzastkaProbna.java
@@ -0,0 +1,21 @@
+package interfejs;
+
+public class CzastkaProbna {
+	
+	int x;
+	int y;
+	double masa;
+	double ladunek;
+	double vx; //prêdkoœæ x
+	double vy; // prêdkoœæ y
+
+	public CzastkaProbna(int x, int y, double m, double q, double vx, double vy) {
+		this.x = x;
+		this.y = y;
+		this.masa = m;
+		this.ladunek = q;
+		this.vx = vx;
+		this.vy = vy;
+	}
+
+}
---
+++ src/interfejs/CzastkaStacjonarna.java
@@ -0,0 +1,15 @@
+package interfejs;
+
+public class CzastkaStacjonarna {
+	
+	int x;
+	int y;
+	double ladunek;
+	
+	public CzastkaStacjonarna(int x, int y, double q) {
+		this.x = x;
+		this.y = y;
+		this.ladunek = q;
+	}
+
+}
